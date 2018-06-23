5aa5f8aa7a109a741de3f2063c757463ec904c2a
TomikuraTakahiro <tomikura0320@gmail.com>
2018-06-23 19:06:53 +0900

    DBをsqliteからmysqlへ変更

--- sake/sake/settings.py
+++ sake/sake/settings.py
@@ -80,8 +80,12 @@ LOGIN_REDIRECT_URL = 'sake_web:top'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nihonsyu',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
